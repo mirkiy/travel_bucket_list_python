0de2b2f036b6a9f09a6762ead446097b0173fc3e
mirkiy <mirizachar@gmail.com>
2021-12-10 14:59:29 +0000

    classes created

---
+++ models/city.py
@@ -0,0 +1,9 @@
+class City:
+
+    def __init__(self, country, name, population, review, sights, id=None):
+        self.country = country
+        self.name = name
+        self.population = population
+        self.review = review
+        self.sights = sights
+        self.id = id
---
+++ models/country.py
@@ -0,0 +1,6 @@
+class Country:
+
+    def __init__(self, name, capital, id=None):
+        self.name = name
+        self.capital = capital
+        self.id = id
