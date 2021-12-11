725ddb11636b3a4052d6a4ee1ffb8a6a91ad3ef0
mirkiy <mirizachar@gmail.com>
2021-12-11 16:18:48 +0000

    modified country_repo

--- repositories/city_repository.py
+++ repositories/city_repository.py
@@ -1,1 +1,41 @@
 from db.run_sql import run_sql
+
+from models.city import City
+from models.country import Country
+import repositories.country_repository as country_repository
+
+
+def save(city):
+    sql = "INSERT INTO cities (country_id, name, population, review, sights) VALUES (%s, %s, %s, %s, %s) RETURNING id"
+    values = [city.country.id, city.name,
+              city.population, city.review, city.sights]
+    results = run_sql(sql, values)
+    city.id = results[0]['id']
+    return city
+
+
+def select_all():
+    cities = []
+
+    sql = "SELECT * FROM cities"
+    results = run_sql(sql)
+
+    for row in results:
+        country = country_repository.select(row['country_id'])
+        city = City(country, row['name'],
+                    row['population'], row['review'], row["sights"], row['id'])
+        cities.append(city)
+    return cities
+
+
+def select(id):
+    city = None
+    sql = "SELECT * FROM books WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        country = country_repository.select(result['country_id'])
+        city = City(country, result['name'],
+                    result['population'], result['review'], result["sights"], result['id'])
+    return city
--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -1,1 +1,67 @@
 from db.run_sql import run_sql
+
+from models.country import Country
+from models.city import City
+
+
+def save(country):
+    sql = "INSERT INTO countries (name, capital) VALUES (%s, %s) RETURNING *"
+    values = [country.name, country.capital]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    country.id = id
+    return country
+
+
+def select_all():
+    countries = []
+
+    sql = "SELECT * FROM countries"
+    results = run_sql(sql)
+
+    for row in results:
+        country = Country(row['name'], row['capital'], row['id'])
+        countries.append(country)
+    return countries
+
+
+def select(id):
+    country = None
+    sql = "SELECT * FROM countries WHERE id =%s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        country = Country(result['name'], result['capital'], result['id'])
+    return country
+
+
+def delete_all():
+    sql = "DELETE FROM countries"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE FROM countries WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def update(country):
+    sql = "UPDATE countries SET (name, capital) = (%s, %s) WHERE id =%s"
+    values = [country.name, country.capital, country.id]
+    run_sql(sql, values)
+
+
+def cities(country):
+    cities = []
+
+    sql = "SELECT * FROM cities WHERE country_id = %s"
+    values = [country.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        city = City(row['country_id'], row['name'],
+                    row['population'], row['review'], row['sight'], row['id'])
+        cities.append(city)
+    return cities
