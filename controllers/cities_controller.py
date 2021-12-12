c6389a787ba61af47703cf1e233cfcf7cd4b8dc5
mirkiy <mirizachar@gmail.com>
2021-12-12 19:02:06 +0000

    post route for cities

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from werkzeug.utils import redirect
 from models.city import City
 # from models.country import Country
 
@@ -23,3 +24,19 @@ def cities():
 def new_city():
     countries = country_repository.select_all()
     return render_template("cities/new_city.html", all_countries=countries)
+
+
+# CREATE
+# POST '/cities'
+@cities_blueprint.route("/cities", methods=['POST'])
+def create_new_city():
+    country_id = request.form['country_id']
+    name = request.form['name']
+    population = request.form['population']
+    review = request.form['review']
+    sights = request.form['sights']
+    visited = request.form['visited']
+    country = country_repository.select(country_id)
+    city = City(country, name, population, review, sights, visited)
+    city_repository.save(city)
+    return redirect('/cities')
