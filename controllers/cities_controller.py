f547b60be68e5860017009e122c78ea5ce81160e
mirkiy <mirizachar@gmail.com>
2021-12-12 18:32:35 +0000

    route to new city form created

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -4,14 +4,22 @@ from models.city import City
 # from models.country import Country
 
 import repositories.city_repository as city_repository
-
+import repositories.country_repository as country_repository
 
 cities_blueprint = Blueprint("cities", __name__)
 
 
-# homepage route
+# index route
 # GET '/cities'
 @cities_blueprint.route("/cities")
 def cities():
     cities = city_repository.select_all()
     return render_template("cities/index.html", all_cities=cities)
+
+# GET '/cities/new_city'
+
+
+@cities_blueprint.route("/cities/new_city", methods=['GET'])
+def new_city():
+    countries = country_repository.select_all()
+    return render_template("cities/new.html", all_countries=countries)
