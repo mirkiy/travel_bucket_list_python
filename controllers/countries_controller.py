03a70c7d37407043c2c8b774c6399d1b3e5728f7
mirkiy <mirizachar@gmail.com>
2021-12-12 22:30:03 +0000

    countries controller - all countries  and index html created

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
-# from flask import Blueprint
+from flask import Blueprint
 from models.country import Country
 
-# import repositories.country_repository as country_repository
+import repositories.country_repository as country_repository
 
-# country_blueprint = Blueprint("countries", __name__)
+countries_blueprint = Blueprint("countries", __name__)
+
+
+# index route
+# GET '/countries'
+@countries_blueprint.route("/countries")
+def countries():
+    countries = country_repository.select_all()
+    return render_template("countries/index.html", all_countries=countries)
