e1549cd036bf1ea7613917883f5d3fd0f1384504
mirkiy <mirizachar@gmail.com>
2021-12-13 10:04:58 +0000

    post route in country controllers created

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.country import Country
 
@@ -15,7 +15,21 @@ def countries():
     return render_template("countries/index.html", all_countries=countries)
 
 # GET 'countries/new_country'
+
+
 @countries_blueprint.route("/countries/new_country", methods=['GET'])
 def new_country():
     countries = country_repository.select_all()
     return render_template("countries/new_country.html", all_countries=countries)
+
+# CREATE
+# POST '/countries'
+
+
+@countries_blueprint.route("/countries", methods=['POST'])
+def create_country():
+    name = request.form['name']
+    capital = request.form['capital']
+    country = Country(name, capital)
+    country_repository.save(country)
+    return redirect('/countries')
